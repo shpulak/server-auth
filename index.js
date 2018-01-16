67292ff02f52c6f2cca9c4f17938883562c09481
Shashwat <shashwat.pulak@gmail.com>
2018-01-17 01:19:58 +0530

    Add Mongoose config and local mongodb connection

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-
+const router = require('./router');
+const mongoose = require('mongoose');
 const app = express();
+
+// DB Setup
+mongoose.connect('mongodb://localhost:auth/auth');
 
 // App setup
 app.use(morgan('combined'));
 app.use(bodyParser.json({type:'*/*'}));
-
+router(app);
 // Server Setup
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
