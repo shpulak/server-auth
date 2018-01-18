064b9e57b9ca7d6ce4c2139bb26930736410d2fa
Shashwat <shashwat.pulak@gmail.com>
2018-01-19 01:54:00 +0530

    Add SignIn Auth flow

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,15 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function (req, res, next) {
+    res.send({ token: tokenForUser(req.user) });
+};
 
 exports.signup = function (req, res, next) {
     // res.send({success:'true'});
@@ -6,7 +17,7 @@ exports.signup = function (req, res, next) {
     const password = req.body.password;
 
     if (!email || !password) {
-        return res.status(422).send({error:'You must provide email and password'});
+        return res.status(422).send({ error: 'You must provide email and password' });
     }
     User.findOne({ email: email }, function (err, existingUser) {
         if (err) { return next(err); };
@@ -20,10 +31,10 @@ exports.signup = function (req, res, next) {
             password: password
         });
 
-        user.save(function(err){
-            if(err){return next(err);};
+        user.save(function (err) {
+            if (err) { return next(err); };
 
-            res.json({success:'true'});
+            res.json({ token: tokenForUser(user) });
         });
     })
 }
